dfceb6988cd37e9041de4deacbb9ab0e8e46e9c5
Mihail <zone.man.2017@mail.ru>
2025-12-15 17:59:15 +0600

    Реализован ProductAdapter

---
+++ app/src/main/java/com/sigma/androidnetworkproducts/adapters/ProductAdapter.java
@@ -0,0 +1,55 @@
+package com.sigma.androidnetworkproducts.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.sigma.androidnetworkproducts.R;
+import com.sigma.androidnetworkproducts.models.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
+
+    private List<Product> products = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_product, parent, false);
+        return new ProductViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
+        Product product = products.get(position);
+        holder.tvTitle.setText(product.getTitle());
+        holder.tvPrice.setText(String.format("$%.2f", product.getPrice()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return products.size();
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+        notifyDataSetChanged();
+    }
+
+    static class ProductViewHolder extends RecyclerView.ViewHolder {
+        TextView tvTitle, tvPrice;
+
+        public ProductViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvPrice = itemView.findViewById(R.id.tvPrice);
+        }
+    }
+}
