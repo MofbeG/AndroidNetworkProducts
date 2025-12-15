962fee0ca0571fa8225fd80588e3d24bf7dcdef2
Mihail <zone.man.2017@mail.ru>
2025-12-15 17:56:01 +0600

    Добавлена модель Product

---
+++ app/src/main/java/com/sigma/androidnetworkproducts/models/Product.java
@@ -0,0 +1,26 @@
+package com.sigma.androidnetworkproducts.models;
+
+public class Product {
+    private int id;
+    private String title;
+    private double price;
+    private String thumbnail;
+    private String description;
+
+    public Product() {}
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+
+    public String getThumbnail() { return thumbnail; }
+    public void setThumbnail(String thumbnail) { this.thumbnail = thumbnail; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+}
